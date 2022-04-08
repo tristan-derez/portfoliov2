a2cf3c66f74933ea667940490ffe7b2a959ca446
dreyzu <tristan.derez@gmail.com>
2022-04-08 18:37:25 +0200

    remove unused files

--- src/components/Show.tsx
+++
@@ -1,6 +0,0 @@
-import { WithChildren } from "@pastable/core";
-import { ReactElement } from "react";
-
-export function Show({ cond, children }: WithChildren & { cond: boolean }): ReactElement | null {
-    return cond ? (children as ReactElement) : null;
-}
