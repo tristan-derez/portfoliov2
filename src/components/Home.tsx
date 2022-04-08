ad3b137ccad57d9fe2779aa804f7191a04cb9170
dreyzu <tristan.derez@gmail.com>
2022-04-08 20:46:24 +0200

    add import for navbar component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,10 +3,12 @@ import "../App.css";
 import { Flex } from "@chakra-ui/react";
 
 import { Cursor } from "./Cursor";
+import { Navbar } from "./Navbar";
 
 export const Home = () => {
     return (
         <Flex direction="column">
+            <Navbar />
             <Cursor />
         </Flex>
     );
