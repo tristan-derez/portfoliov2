e40c225313c1151f3160bb75604874fd7db4b9cd
dreyzu <tristan.derez@gmail.com>
2022-04-10 15:50:53 +0200

    add aboutme improt

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,11 +4,13 @@ import { Flex } from "@chakra-ui/react";
 
 import { Cursor } from "./Cursor";
 import { Navbar } from "./Navbar";
+import { AboutMe } from "./AboutMe";
 
 export const Home = () => {
     return (
         <Flex direction="column">
             <Navbar />
+            <AboutMe />
             <Cursor />
         </Flex>
     );
