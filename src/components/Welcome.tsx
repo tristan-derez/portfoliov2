29ee144fe14805f4e5a4b25c56b3fb496d7d1d63
Tristan Derez <92602528+dreyzu@users.noreply.github.com>
2022-04-20 11:38:38 +0200

    fix width

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,47 +1,37 @@
 import { Flex, Text } from "@chakra-ui/react";
-import { BouncingArrow } from "./BoucingArrow";
+// import { BouncingArrow } from "./BoucingArrow";
 
 export const Welcome = () => {
     return (
-        <Flex
-            minH="100vh"
-            justifyContent="center"
-            alignItems="center"
-            fontFamily="HarmondSemi"
-            letterSpacing="-0.8px"
-            px="40px"
-        >
-            <Flex direction="column" flexWrap="wrap" whiteSpace="nowrap" fontWeight="bold">
+        <Flex minH="100vh" justifyContent="center" alignItems="center" fontFamily="HarmondSemi" letterSpacing="-0.8px">
+            <Flex direction="column" fontWeight="bold" h="inherit" w={["300px", "350px", "500px", "750px"]}>
                 <Flex>
-                    <Text fontSize={["2.5rem", "3.5rem", "4.5rem", "6.5rem"]} lineHeight="1">
+                    <Text fontSize={["40px", "56px", "80px", "115px"]} lineHeight="1">
                         Bienvenue
                     </Text>
                 </Flex>
-                <Flex justifyContent={["flex-start", "flex-end", "center"]}>
-                    <Text fontSize={["2.5rem", "3.5rem", "4.5rem", "6.5rem"]} lineHeight="1">
+                <Flex justifyContent={["flex-start", "flex-start", "flex-end"]}>
+                    <Text fontSize={["40px", "56px", "80px", "115px"]} lineHeight="1">
                         sur le portfolio
                     </Text>
                 </Flex>
                 <Flex>
-                    <Text fontSize={["2.5rem", "3.5rem", "4.5rem", "6.5rem"]} lineHeight="1">
-                        de{" "}
-                        <Text display="inline-flex" color="orange">
-                            Tristan Derez
-                        </Text>
-                    </Text>
+                    <Flex fontSize={["40px", "56px", "80px", "115px"]} lineHeight="1">
+                        de <span>Tristan Derez</span>
+                    </Flex>
                 </Flex>
-                <Flex alignSelf="flex-end">
+                <Flex justifyContent="flex-end">
                     <Text
-                        fontSize={["4rem", "3.5rem", "5.5rem", "6.5rem"]}
+                        fontSize={["4rem", "3.5rem", "5.5rem", "7.1rem"]}
                         lineHeight={["2", "2", "1"]}
                         _hover={{ transform: "rotate(15deg)", cursor: "crosshair" }}
                     >
                         ✌️
                     </Text>
                 </Flex>
-            </Flex>
-            <Flex position="absolute" bottom="20px" fontSize="30px">
-                <BouncingArrow />{" "}
+                {/* <Flex fontSize="30px" position="relative" bottom="0" margin="0 auto">
+                    <BouncingArrow />{" "}
+                </Flex> */}
             </Flex>
         </Flex>
     );
