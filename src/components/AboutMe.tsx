8e6e3e3b0ff58be8e2af488cf10b51895630bfb7
Tristan Derez <92602528+dreyzu@users.noreply.github.com>
2022-04-20 11:38:02 +0200

    add hover bg on button and change text

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -3,30 +3,33 @@ import { Box, Flex, Heading, Text, chakra, useColorModeValue, Button, Center, Li
 export const AboutMe = ({ ...props }) => {
     return (
         <Flex
-            px="40px"
             flexDirection={["column", "column", "column", "row-reverse"]}
             alignItems="center"
             justifyContent="center"
+            py="50px"
             {...props}
             id="about"
         >
-            <Box pt={["20px", "20px", "20px", 0]} textAlign="center">
-                <Heading as="h1" fontFamily="Kirang Haerang" fontSize="50px">
-                    About me
+            <Box pt={["20px", "20px", "20px", 0]} textAlign="center" w={["300px", "350px", "500px", "750px"]}>
+                <Heading as="h1" fontFamily="Zen Kaku Gothic Antique" fontSize="20px">
+                    A propos
                 </Heading>
+                <div className="separator"></div>
                 <Text fontFamily="Zen Kaku Gothic Antique" fontSize="20px" pt="15px">
-                    Bonjour ! <br />
-                    Suite à l'apprentissage en autodidacte du développement Front-End sur
-                    <AboutMeLink href="https://codecademy.com">Codecademy</AboutMeLink>, je me lance dans une formation
-                    <AboutMeLink href="https://openclassrooms.com">OpenClassrooms</AboutMeLink> en alternance pour
-                    consolider et faire certifier mes connaissances.
-                    <br />
-                    Je cherche donc une alternance d'un an, au rythme de 3-4 jours par semaine en entreprise.
+                    Étudiant Développeur Front-end, je suis à la recherche d'une entreprise pour m'accueillir en
+                    alternance d'un an, au rythme de 3/4 jours en entreprise et le reste en formation avec
+                    <AboutMeLink href="https://www.openclassrooms.com">OpenClassrooms</AboutMeLink>.
                 </Text>
                 <Center pt="15px">
                     <Link href="/cv_derez.pdf" download>
-                        <Button borderRadius="3xl" px="40px" py="10px" variant="outline" borderColor="black">
-                            DOWNLOAD MY RESUME
+                        <Button
+                            borderRadius="30px"
+                            mt="40px"
+                            padding="15px 50px"
+                            variant="outline"
+                            _hover={{ bg: "orange" }}
+                        >
+                            TELECHARGER CV
                         </Button>
                     </Link>
                 </Center>
@@ -43,10 +46,6 @@ const AboutMeLink = ({ children, ...props }) => {
             cursor="pointer"
             pl="5px"
             _hover={{
-                backgroundImage: "url('/images/trace-peinture-orange.png')",
-                backgroundSize: "cover",
-                backgroundRepeat: "round",
-                fontWeight: "bold",
                 color: { importantWord },
             }}
             target="_blank"
