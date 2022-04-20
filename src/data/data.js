5b9734a9734ebfa2cc84a3eae2c8a86800b66c15
Tristan Derez <92602528+dreyzu@users.noreply.github.com>
2022-04-21 01:24:50 +0200

    create data

---
+++ src/data/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        imageUrl: "/images/Podrequest.png",
+        imageAlt: "Website's screenshot",
+        stackUsed: "HTML, CSS, JS",
+        title: "Podrequest Pre-launch landing page",
+        description:
+            "In this project, you'll use the GitHub users API to pull profile data and display it. It's a great challenge if you're looking to practice working with a 3rd-party API.",
+        adress: "https://podrequest.vercel.app/",
+        code: "",
+    },
+    {
+        imageUrl: "/images/Devfinder.png",
+        imageAlt: "Website's screenshot",
+        stackUsed: "React, Chakra-ui, API",
+        title: "Devfinder",
+        description:
+            "Création d'une interface permettant avec l'API de GitHub de récuperer les informations d'un utilisateur et de les afficher. Challenge Frontendmentor avec instructions spécifiques, visible directement sur mon GitHub.",
+        adress: "https://devfinder-dreyzu.vercel.app/",
+        code: "https://github.com/dreyzu/devfinder",
+    },
+    {
+        imageUrl: "/images/Equalizer.png",
+        imageAlt: "placeholder",
+        stackUsed: "React, Chakra-UI",
+        title: "Equalizer Landing Page",
+        description:
+            "In this project, you'll use the GitHub users API to pull profile data and display it. It's a great challenge if you're looking to practice working with a 3rd-party API.",
+        adress: "https://equalizer-landingpage.vercel.app/",
+        code: "https://github.com/dreyzu/equalizer-landingpage",
+    },
+    {
+        imageUrl: "/images/Equalizer.png",
+        imageAlt: "placeholder",
+        stackUsed: "React, Chakra-UI",
+        title: "Equalizer Landing Page",
+        description:
+            "In this project, you'll use the GitHub users API to pull profile data and display it. It's a great challenge if you're looking to practice working with a 3rd-party API.",
+        adress: "https://equalizer-landingpage.vercel.app/",
+        code: "https://github.com/dreyzu/equalizer-landingpage",
+    },
+];
