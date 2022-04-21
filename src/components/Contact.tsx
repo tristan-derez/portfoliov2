64981e56a3e22e08c0b83c43e5b52b5e2b5cadb9
Tristan Derez <92602528+dreyzu@users.noreply.github.com>
2022-04-21 22:58:33 +0200

    import useToast

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,23 +1,30 @@
-import { Box, Flex, Heading, Text, Button, Center, Link } from "@chakra-ui/react";
+//chakra
+import { Box, Flex, Heading, useToast } from "@chakra-ui/react";
+
+// components
 import { ContactForm } from "./ContactForm";
 
+//icons
+import { IoCheckmarkDoneCircleSharp } from "react-icons/io5";
+
 export const Contact = ({ ...props }) => {
+    const toast = useToast();
     return (
         <Flex
             alignItems="center"
             justifyContent="center"
             minH="100vh"
-            py="50px"
             {...props}
             fontFamily="Raleway"
             fontWeight="400"
-            id="about"
+            id="contact"
         >
             <Box pt={["20px", "20px", "20px", 0]} textAlign="center" w={["300px", "350px", "750px", "900px"]}>
-                <Heading as="h1" fontFamily="Raleway" fontSize="20px">
-                    A propos
+                <Heading as="h2" fontFamily="Raleway" fontSize="20px">
+                    Contact
                 </Heading>
                 <div className="separator"></div>
+
                 <ContactForm />
             </Box>
         </Flex>
