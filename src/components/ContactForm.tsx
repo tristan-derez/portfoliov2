98ad99cb5cc4c94a0b30458a5dc67d173a374d1d
Tristan Derez <92602528+dreyzu@users.noreply.github.com>
2022-04-21 22:58:58 +0200

    add useToast function to confirm the mail was succesfully sent

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,20 +1,45 @@
+//chakra
+import { Flex, Button, Icon, useToast, FormLabel } from "@chakra-ui/react";
+
+//formspree
 import { useForm, ValidationError } from "@formspree/react";
+
+//icons
+import { IoCheckmarkDoneCircleSharp } from "react-icons/io5";
 
 export const ContactForm = () => {
     const [state, handleSubmit] = useForm("mayvbqzn");
+    const toast = useToast();
     if (state.succeeded) {
-        return <p>Message envoyé</p>;
+        // return chakra toast
     }
     return (
         <form onSubmit={handleSubmit}>
-            <label htmlFor="email">Email Address</label>
-            <input id="email" type="email" name="email" />
-            <ValidationError prefix="Email" field="email" errors={state.errors} />
-            <textarea id="message" name="message" />
-            <ValidationError prefix="Message" field="message" errors={state.errors} />
-            <button type="submit" disabled={state.submitting}>
-                Submit
-            </button>
+            <Flex direction="column" gap="20px" color="orange" w={["300px", "327px", "573px", "730px"]}>
+                <FormLabel htmlFor="email">Email</FormLabel>
+                <input id="email" type="email" name="email" />
+                <ValidationError prefix="Email" field="email" errors={state.errors} />
+                <textarea id="message" name="message" />
+                <ValidationError prefix="Message" field="message" errors={state.errors} />
+                <Button
+                    type="submit"
+                    disabled={state.submitting}
+                    onClick={() => {
+                        toast({
+                            position: "bottom-right",
+                            render: () => (
+                                <Button color="white" padding={6} bg="#4BB543" margin="10px" fontFamily="Raleway">
+                                    Message envoyé
+                                    <Icon as={IoCheckmarkDoneCircleSharp} />
+                                </Button>
+                            ),
+                            isClosable: true,
+                        });
+                    }}
+                >
+                    Submit
+                </Button>
+            </Flex>
         </form>
     );
 };
