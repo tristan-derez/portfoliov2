5c715830b534cca1235bed56e67e635d1045aafb
Tristan Derez <92602528+dreyzu@users.noreply.github.com>
2022-04-26 18:02:38 +0200

    add link to social medias and a link to message me

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,11 +3,12 @@ import { Box, Flex, Heading, Text, Link } from "@chakra-ui/react";
 
 //icons
 import { IoCheckmarkDoneCircleSharp } from "react-icons/io5";
+import { notifyManager } from "react-query";
 
 export const Contact = ({ ...props }) => {
     return (
         <Flex
-            alignItems="center"
+            mt="300px"
             justifyContent="center"
             minH="100vh"
             {...props}
@@ -15,14 +16,35 @@ export const Contact = ({ ...props }) => {
             fontWeight="400"
             id="contact"
         >
-            <Box pt={["20px", "20px", "20px", 0]} textAlign="center" w={["300px", "375px", "600px", "730px"]}>
+            <Box textAlign="center" w={["300px", "375px", "600px", "730px"]}>
                 <Heading as="h2" fontFamily="Raleway" fontSize="22px">
                     Contact
                 </Heading>
                 <div className="separator"></div>
-                <Text pt="15px">
-                    Laissez moi un <Link>message</Link>
-                </Text>
+                <Box py={["150px", "250px"]} fontSize={["22px", "30px"]} fontWeight="bold">
+                    <Link
+                        href="mailto:tristan.derez@gmail.com"
+                        textDecoration="none"
+                        _hover={{ textDecoration: "none" }}
+                    >
+                        Laissez moi un <span className="importantName">message</span>
+                    </Link>
+                </Box>
+                <div className="separator"></div>
+                <Flex gap="20px" justifyContent="center" pt="15px">
+                    <Link href="https://www.linkedin.com/in/tristan-derez/" isExternal>
+                        Linkedin
+                    </Link>
+                    <Link href="https://www.linkedin.com/in/tristan-derez/" isExternal>
+                        GitHub
+                    </Link>
+                    <Link href="https://www.linkedin.com/in/tristan-derez/" isExternal>
+                        Twitter
+                    </Link>
+                    <Link href="https://www.linkedin.com/in/tristan-derez/" isExternal>
+                        Instagram
+                    </Link>
+                </Flex>
             </Box>
         </Flex>
     );
