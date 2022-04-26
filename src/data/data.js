23f546d90e4a8ac801515fa0c7c7c1bdd68d5999
Tristan Derez <92602528+dreyzu@users.noreply.github.com>
2022-04-26 16:23:12 +0200

    update data

--- src/data/data.js
+++ src/data/data.js
@@ -4,8 +4,7 @@ export const data = [
         imageAlt: "Website's screenshot",
         stackUsed: "HTML, CSS, JS",
         title: "Podrequest Pre-launch landing page",
-        description:
-            "In this project, you'll use the GitHub users API to pull profile data and display it. It's a great challenge if you're looking to practice working with a 3rd-party API.",
+        description: `Le défi était de réaliser une landing page "Early access" à partir d'une maquette fournie par FrontEndMentor. Message d'erreur personnalisé`,
         adress: "https://podrequest.vercel.app/",
         code: "",
     },
@@ -24,8 +23,7 @@ export const data = [
         imageAlt: "placeholder",
         stackUsed: "React, Chakra-UI",
         title: "Equalizer Landing Page",
-        description:
-            "In this project, you'll use the GitHub users API to pull profile data and display it. It's a great challenge if you're looking to practice working with a 3rd-party API.",
+        description: "Le défi était de réaliser une landing page à partir d'une maquette fournie par FrontEndMentor",
         adress: "https://equalizer-landingpage.vercel.app/",
         code: "https://github.com/dreyzu/equalizer-landingpage",
     },
