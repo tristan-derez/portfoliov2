2478d0a008d1119d9e4cec19db7059ba732dedbf
Tristan Derez <92602528+dreyzu@users.noreply.github.com>
2022-04-26 16:05:33 +0200

    remove custom navbar on mobile, add theme switcher on mobile device

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ export const Navbar = () => {
             pos="fixed"
             w="100%"
             maxWidth="inherit"
-            px="40px"
+            px={["6px", "6px", "40px"]}
             py="10px"
             color={useColorModeValue("black", "white")}
             alignItems="center"
@@ -40,66 +40,13 @@ export const Navbar = () => {
                 <CustomLink href="#about">A propos</CustomLink>
                 <CustomLink href="#projects">Projets</CustomLink>
                 <CustomLink href="#contact">Contact</CustomLink>
-                <Flex alignSelf="center">
-                    <ToggleButton onClick={toggleColorMode}>
-                        {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
-                    </ToggleButton>
-                </Flex>
             </Flex>
-            <Flex>
+            <Flex alignSelf="center" pl="30px">
                 {/* Hamburger Icon/Open Menu Button */}
-                <IconButton
-                    aria-label="open menu"
-                    icon={<GiHamburgerMenu />}
-                    display={["flex", "flex", "none", "none"]}
-                    onClick={() => changeDisplay("flex")}
-                    _hover={{ bg: "none" }}
-                    bg="none"
-                    color="orange"
-                />
-            </Flex>
-            {/* Navbar on mobile device */}
-            <Flex
-                w="100vw"
-                h="100vh"
-                zIndex={20}
-                inset="0"
-                flexDir="column"
-                display={display}
-                color={useColorModeValue("black", "white")}
-                background={useColorModeValue("white", "#1A202C")}
-                position="absolute"
-            >
-                <Flex justify="flex-end">
-                    {/* Close Icon Button */}
-                    <IconButton
-                        aria-label="close menu"
-                        position="absolute"
-                        top="10px"
-                        right="40px"
-                        size="lg"
-                        h="40px"
-                        w="40px"
-                        icon={<AiOutlineClose />}
-                        onClick={() => changeDisplay("none")}
-                        _hover={{ bg: "none" }}
-                        bg="none"
-                    />
-                </Flex>
-                <Flex direction="column" alignItems="center" pt="100px" gap="25px" fontFamily="Raleway">
-                    <CustomLink href="#" onClick={() => changeDisplay("none")}>
-                        Home
-                    </CustomLink>
-                    <CustomLink href="#About" onClick={() => changeDisplay("none")}>
-                        A propos
-                    </CustomLink>
-                    <CustomLink href="#Projects" onClick={() => changeDisplay("none")}>
-                        Projets
-                    </CustomLink>
-                    <CustomLink href="#Contact" onClick={() => changeDisplay("none")}>
-                        Contact
-                    </CustomLink>
-                </Flex>
+
+                <ToggleButton onClick={toggleColorMode}>
+                    {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+                </ToggleButton>
             </Flex>
         </Flex>
     );
@@ -130,7 +77,7 @@ const ToggleButton = ({ children, ...props }) => {
             bg="none"
             aria-label="toggle light/dark mode"
             variant="ghost"
-            _hover={{ bg: "none", color: useColorModeValue("#222731", "#90A4D4") }}
+            _hover={{ bg: "none", color: useColorModeValue("orange.300", "orange.400") }}
             _active={{
                 bg: "none",
                 transform: "none",
