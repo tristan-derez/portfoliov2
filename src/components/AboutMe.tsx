4790b32026dd1377730b65828521d47017d56d20
Tristan Derez <92602528+dreyzu@users.noreply.github.com>
2022-05-20 18:54:43 +0200

    change resume link

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -61,7 +61,7 @@ export const AboutMe = ({ ...props }) => {
                             className="slide"
                         ></Flex>
                         <Link
-                            href="/cv_derez_portfolio.pdf"
+                            href="/cv_tristan_derez.pdf"
                             download
                             _hover={{ textDecoration: "none" }}
                             transition="all .35s ease-Out"
