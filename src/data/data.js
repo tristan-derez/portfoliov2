26b2a5ead4b671cc68fbc704f3cce51ad6dbf52c
Tristan Derez <92602528+dreyzu@users.noreply.github.com>
2022-05-30 15:43:42 +0200

    change order of objects

--- src/data/data.js
+++ src/data/data.js
@@ -1,14 +1,5 @@
 export const data = [
     {
-        imageUrl: "/images/Podrequest.png",
-        imageAlt: "Website's screenshot",
-        stackUsed: "HTML, CSS, JS",
-        title: "Podrequest Pre-launch landing page",
-        description: `Le défi était de réaliser une landing page "Early access" à partir d'une maquette fournie par FrontEndMentor. Message d'erreur personnalisé`,
-        adress: "https://podrequest.vercel.app/",
-        code: "",
-    },
-    {
         imageUrl: "/images/Devfinder.png",
         imageAlt: "Website's screenshot",
         stackUsed: "React, Chakra-ui, API",
@@ -17,6 +8,15 @@ export const data = [
             "Création d'une interface permettant avec l'API de GitHub de récuperer les informations d'un utilisateur et de les afficher. Challenge Frontendmentor avec instructions spécifiques, visible directement sur mon GitHub.",
         adress: "https://devfinder-dreyzu.vercel.app/",
         code: "https://github.com/dreyzu/devfinder",
+    },
+    {
+        imageUrl: "/images/Podrequest.png",
+        imageAlt: "Website's screenshot",
+        stackUsed: "HTML, CSS, JS",
+        title: "Podrequest Pre-launch landing page",
+        description: `Le défi était de réaliser une landing page "Early access" à partir d'une maquette fournie par FrontEndMentor. Message d'erreur personnalisé`,
+        adress: "https://podrequest.vercel.app/",
+        code: "",
     },
     {
         imageUrl: "/images/Equalizer.png",
